0c757b7caf43d56ff75a2dc065f981d1f13d9987
Tuyen Trung Le <letrungtuyen2002@gmail.com>
2025-02-24 11:11:56 +0700

    Building our LLM powered by Langchain FullStack Application

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,37 +1,42 @@
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
-import openai
-import os
+from typing import Tuple
+
 from dotenv import load_dotenv
-
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
+from output_parsers import summary_parser, Summary
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello LangChain!")
 
-    api_key = os.getenv("COOL_API_KEY")
+def ice_break_with(name: str) -> Tuple[Summary, str]:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username,mock=True)
 
-    summary_template = """
-    given the Linkedin information {information} about a person I want you to create:
-    1. a short summary
-    2. two interesting facts about them
+    summary_template = """ 
+    Given the LinkedIn information {information} about a person I want you to create:
+    1. A short summary
+    2. Two interesting facts about them
+    \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", openai_api_key=api_key )
-    llm = ChatOllama(model = "llama3")
+    llm = ChatOllama(model="llama3")
 
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/letrungtuyen3101/")
-    res = chain.invoke(input={"information": linkedin_data})
+    # chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | summary_parser
+    res:Summary = chain.invoke(input={"information": linkedin_data})
 
-    print(res)
+    return res, linkedin_data.get("profile_pic_url")
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker Enter")
+    ice_break_with(name = "Le Trung Tuyen - Data Science Student")
 
 
